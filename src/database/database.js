6886c6e44efa98902b6f78b70ea204306e9cc5b3
Nikolas Earl <72423846+nearl2001@users.noreply.github.com>
2024-11-05 16:03:57 -0700

    wire up pizza-logger

--- src/database/database.js
+++ src/database/database.js
@@ -4,9 +4,12 @@ const config = require('../config.js');
 const { StatusCodeError } = require('../endpointHelper.js');
 const { Role } = require('../model/model.js');
 const dbModel = require('./dbModel.js');
+const Logger = require('pizza-logger')
+
 class DB {
   constructor() {
     this.initialized = this.initializeDatabase();
+    this.logger = new Logger(config)
   }
 
   // VUNLERABILITY NOTE. If just anyone can make an Admin account, they can do literally anything lol
@@ -297,6 +300,7 @@ class DB {
   }
 
   async query(connection, sql, params) {
+    this.logger.dbLogger(sql)
     const [results] = await connection.execute(sql, params);
     return results;
   }
--- src/routes/orderRouter.js
+++ src/routes/orderRouter.js
@@ -4,6 +4,9 @@ const { Role, DB } = require('../database/database.js');
 const { authRouter } = require('./authRouter.js');
 const { asyncHandler, StatusCodeError } = require('../endpointHelper.js');
 const metrics = require('../metrics.js')
+const Logger = require('pizza-logger')
+
+const logger = new Logger(config)
 
 const orderRouter = express.Router();
 
@@ -80,6 +83,7 @@ orderRouter.post(
   asyncHandler(async (req, res) => {
     const orderReq = req.body;
     const order = await DB.addDinerOrder(req.user, orderReq);
+    logger.factoryLogger({ diner: { id: req.user.id, name: req.user.name, email: req.user.email }, order })
     const startTime = performance.now()
     const r = await fetch(`${config.factory.url}/api/order`, {
       method: 'POST',
