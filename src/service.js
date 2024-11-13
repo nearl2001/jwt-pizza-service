d54b9fd7c022b62007b087fbf31b6d3b55dbb346
Nikolas Earl <72423846+nearl2001@users.noreply.github.com>
2024-11-12 22:28:01 -0700

    feat(server): install cors and add traffic simulator

--- src/service.js
+++ src/service.js
@@ -6,6 +6,7 @@ const version = require('./version.json');
 const config = require('./config.js');
 const metrics = require('./metrics.js')
 const Logger = require('pizza-logger')
+const cors = require('cors')
 
 const logger = new Logger(config)
 
@@ -15,11 +16,18 @@ app.use(setAuthUser);
 app.use(logger.httpLogger);
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', req.headers.origin || '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
   next();
 });
+
+app.use(cors({
+  origin: 'https://pizza.nikolasearlpizzashop.click',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+}));
 
 const apiRouter = express.Router();
 
